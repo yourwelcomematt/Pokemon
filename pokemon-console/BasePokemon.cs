1afd9ac299f81eaa35a8f23dc3a0e53c99d24443
Matthew Ding <mattding8@gmail.com>
2023-06-21 10:53:50 +1200

    Added BasePokemon class.

---
+++ pokemon-console/BasePokemon.cs
@@ -0,0 +1,15 @@
+namespace Pokemon;
+
+public abstract class BasePokemon
+{
+    public int HP;
+    public int Level;
+    
+    public int Attack;
+    public int SpecialAttack;
+    public int Defense;
+    public int SpecialDefense;
+    public int Speed;
+
+    public Move[] CurrentMoves;
+}
